74b0d25828027b4e72cc162053115b5aa5d8e653
Travis Lilleberg <travis.lilleberg@civicactions.com>
2025-10-02 23:09:13 -0400

    Set homepage to forecast_controller, parsed data, and got data displaying.

---
+++ app/controllers/forecast_controller.rb
@@ -0,0 +1,35 @@
+class ForecastController < ApplicationController
+  # GET /
+  def index
+    forecast = Forecast.new(zip_code: "58102")
+    @weather_data = parse_weather_json(
+      JSON.parse(forecast.get_current_forecast),
+      JSON.parse(forecast.get_daily_forecast)
+    )
+  end
+
+  private
+
+  ##
+  # Extracts the information we actually use:
+  # - temp from current forecast,
+  # - high_temp, low_temp, date from daily forecast for today + 6 days
+  def parse_weather_json(current_json, daily_json)
+    output = { daily: [] }
+    output[:temp] = current_json["data"][0]["temp"]
+
+    i = 0
+    daily_json["data"].each do |d|
+      break if i >= 7
+      output[:daily].push({
+        valid_date: d["valid_date"],
+        high: d["high_temp"],
+        low: d["low_temp"]
+      })
+
+      i += 1
+    end
+
+    output
+  end
+end
--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -10,7 +10,7 @@ class Forecast
     validates_presence_of :zip_code
     validates :zip_code, format: { with: /\A\d{5}\z/, message: "Invalid zipcode" }
 
-    def initialize(attributes)
+    def initialize(attributes = {})
         @zip_code = attributes[:zip_code] if attributes[:zip_code] != nil
     end
 
@@ -19,7 +19,7 @@ class Forecast
     end
 
     def get_daily_forecast
-        send_request("daily")
+        send_request("forecast/daily")
     end
 
     private
@@ -27,19 +27,20 @@ class Forecast
     ##
     # Sends a HTTPS request with provided endpoint.
     def send_request(endpoint)
-        Net::HTTP.get(URI(baseurl + endpoint + "?" + query_params + @zip_code))
+        url = "#{baseurl}#{endpoint}?#{query_params}#{@zip_code}"
+        Net::HTTP.get(URI(url))
     end
 
     ##
     # The base url for the Weatherbit API
     # https://www.weatherbit.io/api/weather-forecast-16-day
     def baseurl
-        "https://api.weatherbit.io/v2.0/forecast/"
+        "https://api.weatherbit.io/v2.0/"
     end
 
     ##
     # Query parameters to pass on the API request.
     def query_params
-        "country=US&units=I&key=" + Rails.application.credentials.weatherbit[:key] + "&postal_code="
+        "country=US&units=I&key=#{Rails.application.credentials.weatherbit[:key]}&postal_code="
     end
 end
